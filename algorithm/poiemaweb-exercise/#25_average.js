b284fbdc4b3101f0c4a716d5b1406c9c200763f3
yhun940731@gmail.com <yhun940731@gmail.com>
2021-01-11 22:55:53 +0900

    fix #25

--- algorithm/poiemaweb-exercise/#25_average.js
+++ algorithm/poiemaweb-exercise/#25_average.js
@@ -2,7 +2,7 @@
 // 인수로 주어진 배열의 평균을 구하는 함수를 완성하라.
 
 function average(array) {
-  return array.reduce((acc, cur, i, arr) => i === arr.length - 1 ? (acc + cur) / arr.length : acc + cur, 0)
+  return array.reduce((acc, cur) => acc + cur, 0) / array.length;
 }
 
 console.log(average([5, 3, 4])); // 4
