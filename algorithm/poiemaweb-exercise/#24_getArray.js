76ee2d9afed2d2cb470049e665d5d71cdf124c41
yhun940731@gmail.com <yhun940731@gmail.com>
2021-01-11 22:52:49 +0900

    fix #24

--- algorithm/poiemaweb-exercise/#24_getArray.js
+++ algorithm/poiemaweb-exercise/#24_getArray.js
@@ -2,7 +2,7 @@
 // 인수로 주어진 배열 arr에서 짝수이고 3보다 큰 수만을 구하여 이를 배열로 반환하는 함수를 작성하라
 
 function getArray(arr) {
-  return arr.filter(v => v > 3 && v % 2 === 0);
+  return arr.filter(v => v > 3 && !(v % 2));
 }
 
 console.log(getArray([1, 2, 3, 4, 5, 6])); // [ 4, 6 ]
