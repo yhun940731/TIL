c7ed2b6ec49c348ba90f7266e73e2b106b6ae5e4
yhun940731@gmail.com <yhun940731@gmail.com>
2021-01-11 23:49:40 +0900

    fix #26, solved #27

--- algorithm/poiemaweb-exercise/#26_findMinDistance.js
+++ algorithm/poiemaweb-exercise/#26_findMinDistance.js
@@ -13,5 +13,5 @@ function findMinDistance(array) {
 }
 
 // 1차원 점의 배열
-var array = [1, 3, 4, 8, 13, 17, 20, 23, 24];
+const array = [1, 3, 4, 8, 13, 17, 20, 23, 24];
 console.log(findMinDistance(array)); // [[3, 4], [23, 24]]
