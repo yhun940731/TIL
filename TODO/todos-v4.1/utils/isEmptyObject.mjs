d3964667e17209877f3efc0aa219ae526e41f58b
yhun940731@gmail.com <yhun940731@gmail.com>
2021-02-04 00:04:34 +0900

    update

--- TODO/todos-v4.1/utils/isEmptyObject.mjs
+++ TODO/todos-v4.1/utils/isEmptyObject.mjs
@@ -1,4 +1,4 @@
-const isPlainObject = o => typeof o === 'object' && o.constructor === Object;
+  const isPlainObject = o => typeof o === 'object' && o.constructor === Object;
 const isEmpty = o => !Object.keys(o).length;
 
 const isEmptyObject = o => isPlainObject(o) && isEmpty(o);
