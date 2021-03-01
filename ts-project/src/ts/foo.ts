8655c3a20304d6a7658cdcf00c6b59d3a29ff8de
yhun940731@gmail.com <yhun940731@gmail.com>
2021-03-01 21:59:59 +0900

    learn TypeScript

--- ts-project/src/ts/foo.ts
+++ ts-project/src/ts/foo.ts
@@ -23,3 +23,42 @@ class Person2 {
 }
 
 console.log('hihihihi!!!!');
+
+// 타입 변환 방법, 변수 선언식에도 타입을 지정하기 보다는 값에 대한 타입으로 변수의 타입을 지정해 주는 것이 낫다.
+const $foo = document.querySelector('input[text]') as HTMLInputElement;
+const val = $foo.value;
+
+const $bar = <HTMLInputElement>document.querySelector('input[text]');
+const va = $bar.value;
+
+
+// 앨리어스
+type TodoType = {
+  id: number
+  content: string
+  completed: boolean
+}
+
+// 인터페이스
+interface Todo {
+  id: number
+  content: string
+  completed: boolean
+}
+
+// const todos: Todo[] = [
+//   {id: 1, content: 'HTML', completed: false}
+// ];
+
+// const addTodo = (todo: Todo) => {
+//   todos = [...todos, todo];
+// };
+
+// 제네릭 형식
+const todos: Array<Todo> = [
+  {id: 1, content: 'HTML', completed: false}
+];
+
+const addTodo = (todo: Todo) => {
+  todos = [...todos, todo];
+};
