5cfd5ff20643f93ac22fbe82e20d4afeb7ea67b5
yhun940731@gmail.com <yhun940731@gmail.com>
2021-06-16 19:28:59 +0900

    [수정] 코드수정

--- algorithm/programmers/시저암호.js
+++ algorithm/programmers/시저암호.js
@@ -3,12 +3,19 @@ function solution(s, n) {
     .split("")
     .map((v) => v.charCodeAt())
     .map((v) => {
-      if (v === 32) return 32;
-      if (v <= 90) return v + n > 90 ? v + n - 26 : v + n;
-      return v + n > 122 ? v + n - 26 : v + n;
+      if (v === 32) return String.fromCharCode(32);
+      if (v <= 90) {
+        return v + n > 90
+          ? String.fromCharCode(v + n - 26)
+          : String.fromCharCode(v + n);
+      }
+      return v + n > 122
+        ? String.fromCharCode(v + n - 26)
+        : String.fromCharCode(v + n);
     })
-    .map((v) => String.fromCharCode(v))
     .join("");
 }
+
+console.log(solution("AB", 1)); // BC
 
 console.log(solution("AB", 1)); // BC
